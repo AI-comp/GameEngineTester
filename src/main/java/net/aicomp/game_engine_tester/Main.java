826eefdedfae1ed19f9efcd1da4b498baf531558
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-11-08 02:13:04 +0900

    Enhance tester.

--- src/main/java/net/aicomp/game_engine_tester/Main.java
+++ src/main/java/net/aicomp/game_engine_tester/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -23,14 +24,15 @@ public class Main {
 	private static final String PAUSE_COMMAND = "p";
 	private static final String UNPAUSE_COMMAND = "u";
 	private static final String LOG_FILE_NAME = "log.txt";
+	private static final String JAR_FILE_NAME = "GameEngineTester.jar";
 
 	public static void main(String[] args) throws ParseException, IOException {
 		Options options = new Options().addOption(GAME_COMMAND, true, "the command to execute a game engine")
 				.addOption(NUM_AI, true, "the number of AI programs")
-				.addOption(NORMAL_AI, true, "work as a normal AI program")
-				.addOption(TIME_OUT_AI, true, "work as a slow AI program which time out")
-				.addOption(PAUSE_COMMAND, true, "work as a pause command")
-				.addOption(UNPAUSE_COMMAND, true, "work as a unpause command");
+				.addOption(NORMAL_AI, false, "work as a normal AI program")
+				.addOption(TIME_OUT_AI, false, "work as a slow AI program which time out")
+				.addOption(PAUSE_COMMAND, false, "work as a pause command")
+				.addOption(UNPAUSE_COMMAND, false, "work as a unpause command");
 		CommandLineParser parser = new DefaultParser();
 		if (!start(parser.parse(options, args))) {
 			HelpFormatter help = new HelpFormatter();
@@ -39,26 +41,26 @@ public class Main {
 	}
 
 	private static boolean start(CommandLine cl) throws IOException {
-		if (!cl.hasOption(NUM_AI)) {
-			return false;
-		}
-		int aiCount = Integer.parseInt(cl.getOptionValue(NUM_AI));
 		File logFile = new File(LOG_FILE_NAME);
 		if (cl.hasOption(GAME_COMMAND)) {
+			if (!cl.hasOption(NUM_AI)) {
+				return false;
+			}
+			int aiCount = Integer.parseInt(cl.getOptionValue(NUM_AI));
 			ArrayList<String> commandAndArgs = Lists.newArrayList(cl.getOptionValue(GAME_COMMAND).split(" "));
 			commandAndArgs.add("-a");
-			commandAndArgs.add("java -jar GameEngineTest.jar -t");
+			commandAndArgs.add("java -jar " + JAR_FILE_NAME + " -t");
 			commandAndArgs.add("-p");
-			commandAndArgs.add("java -jar GameEngineTest.jar -p");
+			commandAndArgs.add("java -jar " + JAR_FILE_NAME + " -p");
 			commandAndArgs.add("-u");
-			commandAndArgs.add("java -jar GameEngineTest.jar -u");
+			commandAndArgs.add("java -jar " + JAR_FILE_NAME + " -u");
 			for (int i = 1; i < aiCount; i++) {
 				commandAndArgs.add("-a");
-				commandAndArgs.add("java -jar GameEngineTest.jar -n");
+				commandAndArgs.add("java -jar " + JAR_FILE_NAME + " -n");
 				commandAndArgs.add("-p");
-				commandAndArgs.add("java -jar GameEngineTest.jar -p");
+				commandAndArgs.add("java -jar " + JAR_FILE_NAME + " -p");
 				commandAndArgs.add("-u");
-				commandAndArgs.add("java -jar GameEngineTest.jar -u");
+				commandAndArgs.add("java -jar " + JAR_FILE_NAME + " -u");
 			}
 			if (logFile.exists()) {
 				logFile.delete();
@@ -67,7 +69,21 @@ public class Main {
 			ProcessBuilder pb = new ProcessBuilder(commandAndArgs);
 			pb.start();
 		} else if (cl.hasOption(NORMAL_AI)) {
+			try (Scanner sc = new Scanner(System.in);) {
+				while (true) {
+					while (!sc.nextLine().startsWith("EOD")) {
+					}
+					System.out.println("DUMMY");
+				}
+			}
 		} else if (cl.hasOption(TIME_OUT_AI)) {
+			try (Scanner sc = new Scanner(System.in);) {
+				while (true) {
+					while (!sc.nextLine().startsWith("EOD")) {
+					}
+					System.out.println("DUMMY");
+				}
+			}
 		} else if (cl.hasOption(PAUSE_COMMAND)) {
 			Files.append("pause\n", logFile, Charset.defaultCharset());
 		} else if (cl.hasOption(UNPAUSE_COMMAND)) {
